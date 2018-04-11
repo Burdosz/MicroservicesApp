843e66185a6df166d55e07ad07bb2a7312391447
Bartosz Brudek <burdosz@MacBook-Pro-2.local>
2018-04-11 23:24:08 +0200

    add other services

---
+++ Library/Controllers/BorrowsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    [Route("api/[controller]")]
+    public class BorrowsController : Controller
+    {
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(int id)
+        {
+            return new string[] {"Opowiesci z Narni", "Wiedzmin"};
+        }
+    }
+}
